8ea115773b23b1cb0db808266e51cb0b70d358e3
Cinthia <163566097+cameli15@users.noreply.github.com>
2025-10-14 22:14:11 -0400

    Create classpg.js

---
+++ classpg.js
@@ -0,0 +1,48 @@
+<html>
+  <head>
+    <title>Contact Page</title>
+    <link rel="stylesheet" href="styling.css" />
+  </head>
+
+  <body>
+    <!--Header Section-->
+    <header id="bar">
+      <h1>Contact Me</h1>
+    </header>
+
+    <!--Nav Bar Section-->
+    <nav id="main-nav">
+      <ul>
+        <li><a href="home.html">Home</a></li>
+        <li><a href="games.html">Game</a></li>
+        <li><a href="contact.html">Contact Me</a></li>
+      </ul>
+    </nav>
+
+    <h1 style="width: 50%; margin: 10">Questions? Reach out!</h1>
+
+    <!--Form Section-->
+    <div class="contactForm">
+      <form id="cForm" onsubmit="return validateForm()">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+        <br /><br />
+
+        <label for="date">Date:</label>
+        <input type="date" id="date" name="date" required />
+        <br /><br />
+
+        <label for="question">Question:</label>
+        <textarea id="question" name="question" required></textarea>
+        <br /><br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+
+    <!-- Footer Section -->
+    <footer id="foot">
+      <p>Created by Cinthia Contreras</p>
+    </footer>
+    <script src="classpagejava.js"></script>
+  </body>
+</html>
